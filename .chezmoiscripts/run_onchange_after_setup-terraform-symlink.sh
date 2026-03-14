ea4d407cbfac91f2110be4b564005f44280642fb
James Nesbitt <james.r.nesbitt@gmail.com>
2026-03-14 15:31:07 +0200

    feat: migrate .pi to XDG compliant path with symlink

--- .chezmoiscripts/run_onchange_after_setup-terraform-symlink.sh
+++ .chezmoiscripts/run_onchange_after_setup-terraform-symlink.sh
@@ -2,3 +2,7 @@
 # Terraform legacy symlink
 mkdir -p ~/.local/share/terraform
 ln -sfn ~/.local/share/terraform ~/.terraform.d
+
+# Pi legacy symlink
+mkdir -p ~/.local/share/pi
+ln -sfn ~/.local/share/pi ~/.pi
